8d9d8c420572840ca637f4355306c37b724ed7fc
Crowdbotics <team@crowdbotics.com>
2023-07-13 16:30:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hvac-equipment-view-42147",
+  url: "https://hvac-equipment-view-42147.botics.co",
+  api: "https://hvac-equipment-view-42147.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
